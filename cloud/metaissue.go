e5892b355505425852fe9cfd38e781585aa0337f
Scott Mitchell <scottmitchell2014@gmail.com>
2024-08-05 12:43:25 -0700

    move models from baton to go-jira

--- cloud/metaissue.go
+++ cloud/metaissue.go
@@ -21,6 +21,43 @@ type EditMetaInfo struct {
 	Fields tcontainer.MarshalMap `json:"fields,omitempty"`
 }
 
+// https://developer.atlassian.com/platform/forge/manifest-reference/modules/jira-custom-field-type/#data-types
+const (
+	TypeString   = "string"
+	TypeArray    = "array"
+	TypeDate     = "date"
+	TypeDateTime = "datetime"
+	TypeNumber   = "number"
+	TypeUser     = "user"
+	TypeGroup    = "group"
+	TypeObject   = "object"
+	TypeOption   = "option"
+)
+
+// MetaDataFields is for tcontainer.MarshalMap in MetaIssueType but used specifically in baton-jira/datacenter
+type MetaDataFields struct {
+	Required        bool     `json:"required"`
+	Schema          Schema   `json:"schema"`
+	Name            string   `json:"name"`
+	Key             string   `json:"key"`
+	HasDefaultValue bool     `json:"hasDefaultValue"`
+	AllowedValues   []Choice `json:"allowedValues,omitempty"`
+}
+
+type Choice struct {
+	Id string `json:"id"`
+	// Could be name or value in most cases
+	Name  string `json:"name,omitempty"`
+	Value string `json:"value,omitempty"`
+}
+
+type Schema struct {
+	Type     string `json:"type"`
+	Custom   string `json:"custom"`
+	CustomId int    `json:"customId"`
+	Items    string `json:"items,omitempty"`
+}
+
 // MetaProject is the meta information about a project returned from createmeta api
 type MetaProject struct {
 	Expand string `json:"expand,omitempty"`
